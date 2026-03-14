f915fc4a53576ddb8c77b8bb9ac4610c6b59120a
Rishi Rana <rishirana012354@gmail.com>
2026-03-14 13:31:38 +0530

    middleware to proxy

--- src/proxy.ts
+++ src/proxy.ts
@@ -1,11 +1,11 @@
-import { NextResponse , NextRequest } from 'next/server'
+import { NextResponse, NextRequest } from 'next/server'
 
 export { default } from "next-auth/middleware"
 import { getToken } from "next-auth/jwt"
 
 
 // This function can be marked `async` if using `await` inside
-export async function middleware(request: NextRequest) {
+export async function proxy(request: NextRequest) {
 
     const token = await getToken({ req: request })
     const url = request.nextUrl
@@ -15,10 +15,8 @@ export async function middleware(request: NextRequest) {
     //         url.pathname.startsWith('/sign-in') ||
     //         url.pathname.startsWith('/sign-up') ||
     //         url.pathname.startsWith('/verify') ||
-    //         url.pathname.startsWith('/') 
-
-    //     )
-    // ) {
+    //         url.pathname.startsWith('/')
+    //     )) {
     //     return NextResponse.redirect(new URL('/dashboard', request.url))
 
     // }
