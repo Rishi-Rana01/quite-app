337cf90fc1ee66eaaf46a1779fc047b6bc8eda0c
Rishi Rana <rishirana012354@gmail.com>
2026-03-25 12:35:00 +0530

    send-message route, get-message-routes

--- src/app/api/get-messages/route.ts
+++ src/app/api/get-messages/route.ts
@@ -10,14 +10,42 @@ export async function GET(request: Request) {
     await dbConnect()
 
     const session = await getServerSession(authOptions)
-        const user: User = session?.user as User
-    
-        if (!session || !session.user) {
+    const user: User = session?.user as User
+
+    if (!session || !session.user) {
+        return Response.json({
+            success: false,
+            message: "Not Authenticate"
+        }, { status: 401 })
+    }
+
+    const userId = new mongoose.Types.ObjectId(user._id);
+    try {
+        const user = await UserModel.aggregate([
+            { $match: { id: userId } },
+            { $unwind: "$messages" },
+            { $sort: { "messages.createdAt": -1 } },
+            { $group: { _id: "$_id", messages: { $push: "$messages" } } }
+        ])
+        if (!user || user.length === 0) {
             return Response.json({
                 success: false,
-                message: "Not Authenticate"
+                message: "User not found or no messages available"
             }, { status: 401 })
         }
-    
-        const userId = new mongoose.Types.ObjectId(user._id);
+
+        return Response.json({
+                success: true,
+                message: "Messages retrieved successfully",
+                data: user[0].messages
+            }, { status: 200 })
+
+
+    } catch (error) {
+        console.error("Error fetching messages:", error);
+        return Response.json({
+            success: false,
+            message: "Error occurred while fetching messages"
+        }, { status: 500 })
+    }
 }
